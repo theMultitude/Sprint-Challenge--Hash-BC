8ebd4afde3274e4fa552ffcd728d472ab043b7e3
Chris Schroeder <os.schroeder@gmail.com>
2019-12-06 09:17:13 -0800

    finished exercises 1 + 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,19 +1,24 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve,)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i, weight in enumerate(weights):
+        num_to_limit = limit - weight  #grab difference in weight
+        num_to_limit_index = hash_table_retrieve(ht, num_to_limit) #get the index of that difference
 
-    return None
+        #check for None and make sure values are in proper order
+        if num_to_limit_loc is not None:
+            if (i > num_to_limit_index):
+                return (i, num_to_limit_index)
+            else:
+                return (num_to_limit_index, i)
+        else:
+            hash_table_insert(ht, weight, i)
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,16 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    #add tickets to hashtable
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    #grab first stop
+    preceding_stop = hash_table_retrieve(hashtable, 'NONE')
+
+    # Create the route array by linking preceding_stop with it's pair
+    for i, nada in enumerate(route):
+        route[i] = preceding_stop
+        preceding_stop = hash_table_retrieve(hashtable, preceding_stop)
 
     return route
